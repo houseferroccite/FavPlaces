ee39450a9ff2cfd4b8a5935ab7043d461b513aa7
Alexei Zimovets <first.man.officila@yandex.ru>
2020-07-11 12:24:19 +0500

    Initial Commit

---
+++ FavPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FavPlaces
+//
+//  Created by Алексей Зимовец on 11.07.2020.
+//  Copyright © 2020 Алексей Зимовец. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
